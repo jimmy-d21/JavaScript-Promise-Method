2d52b3f458e29bce039482fe6f51270607f06173
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-12 22:36:27 +0800

    Wrapping setTimeout inside a Promise

--- JavaScript-Promise-Method.js
+++ JavaScript-Promise-Method.js
@@ -9,3 +9,9 @@ new Promise((_, reject) => reject(new Error("Failed!"))).catch((err) =>
   console.log(err.message),
 );
 // Output: "Failed!"
+
+// 3. Wrapping setTimeout inside a Promise
+const delay = (ms) =>
+  new Promise((resolve) => setTimeout(() => resolve(`Done in ${ms}ms`), ms));
+delay(10).then(console.log);
+// Output: "Done in 10ms"
