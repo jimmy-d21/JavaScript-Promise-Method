26dd137cf70965b2731713770e2a543f72b8fe34
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-12 22:35:38 +0800

    Basic rejected Promise creation

--- JavaScript-Promise-Method.js
+++ JavaScript-Promise-Method.js
@@ -3,3 +3,9 @@
 // 1. Basic successful Promise creation
 new Promise((resolve) => resolve("Success!")).then(console.log);
 // Output: "Success!"
+
+// 2. Basic rejected Promise creation
+new Promise((_, reject) => reject(new Error("Failed!"))).catch((err) =>
+  console.log(err.message),
+);
+// Output: "Failed!"
